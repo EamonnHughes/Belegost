19624df09046302ecb54d7533d9682582b5cd073
arathorn2 <efohughes@gmail.com>
2022-06-11 18:14:53 -0400

    Update HealthPickup.scala

--- core/src/org/eamonn/belegost/HealthPickup.scala
+++ core/src/org/eamonn/belegost/HealthPickup.scala
@@ -1,9 +1,10 @@
 package org.eamonn.belegost
 
 import com.badlogic.gdx.graphics.g2d.PolygonSpriteBatch
+import org.eamonn.belegost.scenes.Game
 import org.eamonn.belegost.util.Location
 
-case class HealthPickup(var location: Location) extends PickUp {
+case class HealthPickup(var location: Location, game: Game) extends PickUp {
   def draw(batch: PolygonSpriteBatch): Unit = {
     batch.draw(
       Belegost.HealthPotion,
@@ -13,4 +14,5 @@ case class HealthPickup(var location: Location) extends PickUp {
       Belegost.screenUnit
     )
   }
+  var corresponding = HealthPotion(game)
 }
