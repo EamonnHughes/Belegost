c0d0f0224df717c785b518a850df684a93613076
arathorn2 <efohughes@gmail.com>
2022-06-22 11:19:36 -0400

    better scrolling

--- core/src/org/eamonn/belegost/scenes/Game.scala
+++ core/src/org/eamonn/belegost/scenes/Game.scala
@@ -21,6 +21,8 @@ import sun.security.ec.point.ProjectivePoint.Mutable
 import scala.collection.mutable
 
 class Game extends Scene {
+  var changingTranslationX = false
+  var changingTranslationY = false
   var keysPressed = List.empty[Int]
   var quit = false
   var pickups = List[PickUp](HealthPickup(Location(24, 5), this))
@@ -32,30 +34,74 @@ class Game extends Scene {
   var everything = List.empty[Entity]
   override def init(): InputAdapter = new GameControl(this)
   var tick = 0.2f
+  def changeX(delta: Float): Unit = {
+    if (
+      Belegost.translationX <
+        ((Geometry.ScreenWidth / 2 / Belegost.screenUnit).toInt - player.location.x)
+    ) {
+      Belegost.translationX = (Belegost.translationX + 1).toInt
+    } else if (
+      Belegost.translationX >
+        ((Geometry.ScreenWidth / 2 / Belegost.screenUnit).toInt - player.location.x)
+    ) {
+      Belegost.translationX = (Belegost.translationX - 1).toInt
+    } else {
+      changingTranslationX = false
+    }
+  }
+  def changeY(delta: Float): Unit = {
+    if (
+      Belegost.translationY <
+        ((Geometry.ScreenHeight / 2 / Belegost.screenUnit).toInt - player.location.y)
+    ) {
+      Belegost.translationY = (Belegost.translationY + 1).toInt
+    } else if (
+      Belegost.translationY >
+        ((Geometry.ScreenHeight / 2 / Belegost.screenUnit).toInt - player.location.y)
+    ) {
+      Belegost.translationY = (Belegost.translationY - 1).toInt
+    } else {
+      changingTranslationY = false
+    }
+
+  }
+
   override def update(delta: Float): Option[Scene] = {
     if (
-      player.location.x < -Belegost.translationX + 7 || player.location.x > -Belegost.translationX + (Geometry.ScreenWidth / Belegost.screenUnit) - 7
+      (
+        player.location.x < -Belegost.translationX + 7 || player.location.x > -Belegost.translationX + (Geometry.ScreenWidth / Belegost.screenUnit) - 7
+      ) && !changingTranslationX
     ) {
-      Belegost.translationX =
-        ((Geometry.ScreenWidth / 2 / Belegost.screenUnit).toInt - player.location.x)
+      changingTranslationX = true
     }
     if (
-      player.location.y < -Belegost.translationY + 7 || player.location.y > -Belegost.translationY + (Geometry.ScreenHeight / Belegost.screenUnit) - 7
+      (
+        player.location.y < -Belegost.translationY + 7 || player.location.y > -Belegost.translationY + (Geometry.ScreenHeight / Belegost.screenUnit) - 7
+      ) && !changingTranslationY
     ) {
-      Belegost.translationY =
-        ((Geometry.ScreenHeight / 2 / Belegost.screenUnit).toInt - player.location.y)
+      changingTranslationY = true
     }
-    if (enemies.length < 1) {
-      enemies = Enemy(Location(10, 10), Location(10, 10), this, 10) :: enemies
+    if (changingTranslationX) {
+      changeX(delta)
     }
-    if (player.moved) {
-      tick -= delta
-    } else { player.update(delta) }
-    if (tick <= 0) {
-      tick = 0.2f
-      enemies.foreach(enemy => enemy.update(delta))
+    if (changingTranslationY) {
+      changeY(delta)
+    }
+    if (!changingTranslationX && !changingTranslationY) {
+      if (enemies.length < 1) {
+        enemies = Enemy(Location(10, 10), Location(10, 10), this, 10) :: enemies
+      }
+      if (player.moved) {
+        tick -= delta
+      } else {
+        player.update(delta)
+      }
+      if (tick <= 0) {
+        tick = 0.2f
+        enemies.foreach(enemy => enemy.update(delta))
 
-      player.moved = false
+        player.moved = false
+      }
     }
     everything = player :: enemies
     if (quit || player.health <= 0) {
