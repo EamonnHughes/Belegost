31526590ab978936742d28b05c590bf121def3f1
arathorn2 <efohughes@gmail.com>
2022-06-24 16:23:21 -0400

    Update Cloak.scala

--- core/src/org/eamonn/belegost/equipment/Cloak.scala
+++ core/src/org/eamonn/belegost/equipment/Cloak.scala
@@ -6,7 +6,7 @@ case class Cloak(game: Game) extends Equipment {
   val aType: ArmorType = ArmorType.Leather
   var mod = aType.mod
 
-  var name = s"${aType.name} Cloak (+ ${mod})"
+  var name = s"${aType.name}-Reinforced Cloak (+ ${mod})"
   def use: Unit = {
     game.player.cloak = Some(this)
   }
