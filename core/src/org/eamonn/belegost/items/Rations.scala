a157389229cab4e7eb6b00cd20559a22ac5425fc
arathorn2 <efohughes@gmail.com>
2022-07-12 11:58:28 -0400

    Update Rations.scala

--- core/src/org/eamonn/belegost/items/Rations.scala
+++ core/src/org/eamonn/belegost/items/Rations.scala
@@ -5,7 +5,8 @@ import org.eamonn.belegost.scenes.Game
 
 case class Rations(game: Game) extends Item {
   def use: Unit = {
-    game.player.hunger += (250 - game.player.hunger) / 10
+    game.player.hunger =
+      (game.player.hunger + ((250 - game.player.hunger) / 10) + 10) max 250
 
   }
   var name = "Rations"
