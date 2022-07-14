8be8b4e387da0fff08090dc49dc71e56d2aa847e
arathorn2 <efohughes@gmail.com>
2022-07-14 10:24:41 -0400

    shop ish

--- core/src/org/eamonn/belegost/Shop.scala
+++ core/src/org/eamonn/belegost/Shop.scala
@@ -1,7 +1,24 @@
 package org.eamonn.belegost
 
 import org.eamonn.belegost.items.Item
+import org.eamonn.belegost.util.Location
 
 import scala.collection.mutable
 
-case class Shop(var inventory: mutable.ListBuffer[(Int, Item)]) {}
+case class Shop(var inventory: mutable.ListBuffer[(Int, Item)]) {
+  var invMenu = NavMenu(
+    {
+      inventory
+        .map({ case (num, ite) =>
+          menuItem(
+            "$" + s"$num ${ite.name}",
+            () => { ite.use }
+          )
+        })
+        .toList
+    },
+    Location(1, (Geometry.ScreenHeight / Belegost.screenUnit).toInt - 1),
+    10,
+    (Geometry.ScreenWidth / Belegost.screenUnit).toInt - 2
+  )
+}
